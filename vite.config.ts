18f54b08ae2107fe790aa21ed14adf926fc3c3f2
alexn16 <alejandro.velasco.monje@gmail.com>
2026-01-26 15:56:51 +0100

    s

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-  },
-})
+  plugins: [react()],
+  base: "/albedo-industries/",
+});
